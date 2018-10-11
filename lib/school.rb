c73cdd5a3a4363d66d9d7435c545bf92e405538d
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-11 19:09:52 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,10 @@ class School
   attr_reader :name, :roster
 
   def add_student(name, grade)
-    @roster[grade] = [name] if @roster[grade] == nil
-    @roster[grade] << name if @roster[grade] != nil
+    if @roster[grade] == nil
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
   end
 end
